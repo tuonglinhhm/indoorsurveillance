8ad365b934fe5f9ea031345859e24388ce6bd2ff
Van-Linh Nguyen <tuonglinhhm@gmail.com>
2022-06-10 10:22:13 +0800

    Indoor Surveillance

---
+++ Data_analysis.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# @brief    Build and evaluate a deep-learning-based indoor localization system
+#           using Wi-Fi fingerprinting
+#
+# @remarks  This work is based on the <a href="https://keras.io/">Keras</a>-based
+#           implementation of the system described in "<a
+#           href="https://arxiv.org/abs/1611.02049v2">Low-effort place
+#           recognition with WiFi fingerprints using deep learning</a>".
+#
+
+
+import itertools
+import numpy as np
+import pandas as pd
+
+
+train_df = (pd.read_csv('../WiFi/TrainingData.csv', header=0))[:19930]
+
+blds = np.unique(train_df[['BUILDINGID']])
+flrs = np.unique(train_df[['FLOOR']])
+
+max_aps = {}
+keys = []
+for bld in blds:
+    for flr in flrs:
+        df = (train_df[(train_df['BUILDINGID']==bld) & (train_df['FLOOR']==flr)]).iloc[:,:520]
+        key = str(bld) + '-' + str(flr)  # dictionary key
+        max_aps[key] = set(df.idxmax(axis=1))
+        keys.append(key)
+
+for pair in itertools.combinations(keys, 2):
+    intersection = max_aps[pair[0]].intersection(max_aps[pair[1]])
+    print("Intersection of %s and %s: %s" % (pair[0], pair[1], str(intersection)))
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# indoorsurveillance
+# This is repository for a signal-based surveillance method
+
+Required 
+ 1. DL
+   - Python 3, Keras
+ 2. MIMO localization
+   - Matlab
+
